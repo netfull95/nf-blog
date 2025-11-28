cb4d5d047dddf9d5fdca68892265a76e73d59b91
NetFull <netfull9x@gmail.com>
2025-11-28 09:54:18 +0700

    update ecosys
    Signed-off-by: NetFull <netfull9x@gmail.com>

---
+++ ecosystem.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+  apps: [
+    {
+      name: "my-next-app",
+      script: "node_modules/next/dist/bin/next",
+      args: "start -p 1995",
+      cwd: "./",
+    }
+  ],
+
+//   apps: [
+//     // First application
+//     {
+//       name: 'SSR',
+//       script: 'server.js',
+//       instances: 0,
+//       exec_mode: 'cluster',
+//       env: {},
+//       env_production: {
+//         NODE_ENV: 'production',
+//       },
+//     },
+//   ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy: {
+    production: {
+      user: 'cc',
+      host: ['10.1.12.221'],
+      ref: 'origin/main',
+      repo: 'git@github.com:netfull95/nf-blog.git',
+      path: '/home/cc/nf-blog',
+      'post-deploy': 'next start -p 1995',
+      ssh_options: ['ForwardAgent=yes']
+    },
+  }
+}
